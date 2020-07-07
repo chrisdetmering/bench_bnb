69ea5cf97560916c9882a8e662c82728401ed96b
froggy <chrisdetmering@gmail.com>
2020-07-06 20:03:37 -0700

    Project setup completed

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+  let root = document.getElementById('root'); 
+
+  ReactDOM.render(<h1>Component1</h1>, root)
+})
